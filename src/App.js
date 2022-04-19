20bfe0f072d5c195bd9fe23c7529d8092f75e329
Leandro Paiva <leandroapaiva@yahoo.com>
2022-04-19 17:48:29 -0300

    erro item 02

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import Content from './components/Content';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    console.log(event.target);
+  }
+
   render() {
+    const { email, password, name } = this.state;
     return (
-      <Content />
+      <Content
+        onChange={ this.onChange }
+        email={ email }
+        password={ password }
+        name={ name }
+      />
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,44 @@ import React, { Component } from 'react';
 
 class Login extends Component {
   render() {
+    const { email, password, name, onChange } = this.props;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <h1>TrybeTunes</h1>
+        <form>
+          <input
+            name="name"
+            onChange={ onChange }
+            placeholder="Name"
+            data-testid="login-name-input"
+            type="text"
+            value={ name }
+          />
+
+          <input
+            name="email"
+            placeholder="Email"
+            type="email"
+            value={ email }
+          />
+
+          <input
+            name="password  "
+            placeholder="Password"
+            type="password"
+            value={ password }
+          />
+
+          <button
+            type="button"
+            data-testid="login-submit-button"
+
+          >
+            Entrar
+          </button>
+
+        </form>
+
       </div>
     );
   }
