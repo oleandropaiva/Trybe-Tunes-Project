13bf970c33f12b3593590085a198cbf8814d5ecb
Leandro Paiva <leandroapaiva@yahoo.com>
2022-04-21 23:34:16 -0300

    iten 03

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      userLogin: '',
+    };
+  }
+
+  componentDidMount() {
+    this.userLogged();
+  }
+
+  userLogged = async () => {
+    this.setState({ loading: true });
+    const userLogin = await getUser();
+    this.setState({
+      loading: false,
+      userLogin: userLogin.name,
+    });
+  }
+
   render() {
+    const { userLogin, loading } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        <h1 data-testid="header-user-name">{ userLogin }</h1>
+        {loading && <Loading />}
+      </header>
     );
   }
 }
