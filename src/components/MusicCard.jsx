9b7143193ee8913462a56809e67c698e88dc335b
Leandro Paiva <leandroapaiva@yahoo.com>
2022-04-26 11:07:45 -0300

    iten 08 fazendo

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import addSong from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
-  render() {
-    const { music } = this.props;
-    return (
-      <div>
-        <p>{ music.trackName }</p>
-        <audio
-          data-testid="audio-component"
-          src={ music.previewUrl }
-          controls
-        >
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>áudio</code>
-        </audio>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicado: false,
+      loading: false,
+    };
   }
+
+     favoriteCheckbox= async () => {
+       this.setState({
+         clicado: true,
+         loading: true,
+       });
+       const { music } = this.props;
+       await addSong(music);
+     }
+
+     render() {
+       const { music } = this.props;
+       const { clicado, loading } = this.state;
+       return (
+         <div>
+           <p>{ music.trackName }</p>
+           <audio
+             data-testid="audio-component"
+             src={ music.previewUrl }
+             controls
+           >
+             <track kind="captions" />
+             O seu navegador não suporta o elemento
+             <code>áudio</code>
+           </audio>
+
+           <label htmlFor="favorita">
+             Favorita
+             <input
+               type="checkbox"
+               id="favorita"
+               name="favorita"
+               data-testid={ `checkbox-music-${music.trackId}` }
+               checked={ clicado }
+               onChange={ this.favoritaCheckbox }
+             />
+           </label>
+           {loading && <Loading />}
+         </div>
+       );
+     }
 }
 
 MusicCard.propTypes = {
-  song: PropTypes.arrayOf/* (PropTypes.object) */,
+  song: PropTypes.arrayOf(PropTypes.object),
 }.isRequired;
 export default MusicCard;
